c7b08813d09a6132c035392d9a76f83a52091248
Unknown <alexjangspammyspam@gmail.com>
2017-10-25 23:57:19 -0700

    Changed API so that you get the current stats of players of specific week

--- test/index.js
+++ test/index.js
@@ -14,51 +14,68 @@ let yahooWRStats = "https://sports.yahoo.com/nfl/stats/weekly/?sortStatId=RUSHIN
 let yahooDEFStats = "https://sports.yahoo.com/nfl/stats/weekly/?sortStatId=RUSHING_YARDS&selectedTable=7&week=7";
 let localhost = "http://localhost:3000";
 
-describe('Sports Stats Server', () => {
-    describe('Test Server Works', () => {
-        it('should return 200', done => {
-            chai.request(app)
-                .get('/')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body['message'].should.equal('done');
-                    done();
-                });
-        });
-        describe('Test Current Scores', () => {
-            it('should return current scores for that week', done => {
-                chai.request(app)
-                    .get('/scores/current')
-                    .end((err, res) => {
-                        res.should.have.status(200);
-                        res.body.gameScores.should.be.a('object');
-                        done();
-                    });
-            })
-        });
-
-
+describe('Test Server Works', () => {
+    it('should return 200', done => {
+        chai.request(app)
+            .get('/')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body['message'].should.equal('done');
+                done();
+            });
     });
+});
 
-    describe('Test Links For Current Stats', () => {
-        it('should return the current week number at the end of the yahoo links', done => {
-            chai.request(app)
-                .get('/stats/current')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.body.should.be.a('array');
-                    let qb = res.body[0],
-                        rb = res.body[1],
-                        wr = res.body[2],
-                        def = res.body[3];
-
-                    assert.equal(yahooQBStats, qb, 'these are not equal!');
-                    assert.equal(yahooRBStats, rb, 'these are not equal!');
-                    assert.equal(yahooWRStats, wr, 'these are not equal!');
-                    assert.equal(yahooDEFStats, def, 'these are not equal!');
-                    done();
-                })
-        })
+describe('Test Current Scores', () => {
+    it('should return current scores for that week', done => {
+        chai.request(app)
+            .get('/scores/current')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.gameScores.should.be.a('object');
+                done();
+            });
     })
 });
 
+describe('Test Links For Current Stats of Positions', () => {
+    it('should return the current stats for the qb position', done => {
+        chai.request(app)
+            .get('/stats/current/qb')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            })
+    });
+
+    it('should return the current stats for the rb position', done => {
+        chai.request(app)
+            .get('/stats/current/rb')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            })
+    });
+
+    it('should return the current stats for the wr position', done => {
+        chai.request(app)
+            .get('/stats/current/wr')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            })
+    });
+
+    it('should return the current stats for the def position', done => {
+        chai.request(app)
+            .get('/stats/current/def')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            })
+    });
+});
